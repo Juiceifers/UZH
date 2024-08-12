1c72cfffc9d87e1dc054fc8d2e3169aae4213fe8
Ishana Rana <ishana.rana@uzh.ch>
2024-08-12 17:45:16 +0200

    oo

---
+++ FS24/ECL/Lecture_notes/lec_13.md
@@ -0,0 +1,129 @@
+# RIP JAKOB JOSEF
+## Automatic ...
+page 88:
+- where do we get higher results? comparing with prev subtitles or by the professional?
+    - [..]
+  
+page 89:
+- crime series subtitles did best
+- comedy (had a lot of puns and special featerus of language): also good
+  - bc training material was comedy
+  - these kinds of comedies, that come weekly, are highly repetitive
+  - difficult puns did not perform that well
+  - probably did not have many cultural references
+- car document: not that good
+
+### Last week: Neural Transform System: NMT
+page 56:
+2) in previous approaches: narrow context to left and right but here its considering the whole sentence all the time
+3) if we have half the sentence already generated => also has an influence on the next word
+
+page 57:
+2) accuracy more of a problem than fluency
+- problem of incompleteness solved
+3) looks good even though there are some mistranslation
+
+page 58:
+- if evaluation is done by sentence by sentence: achieved
+- but if paragraph per paragraph: human still better
+- still true? has MT reached human parity?
+  - deepl, google, textshuttle produce MT that are siglightly better than chatgpt
+  - ..
+
+page 59:
+- how much time needed for post-editing
+- predicted that around 2027 MT will reach human parity
+
+### Machine Translation 4.0; Large Model baded Translation
+page 64:
+- suceeded in being best at latin-german translation
+- but then with CHATgpt not anymore
+  - plus it can also do to english
+  - sort of like a mirical: bc they prob did not specifically train this
+- not quite correct
+  
+page 67:
+- first part in latin, seconf in German
+- still performs well
+
+page 69:
+- suspect taht openai had added this as one of their task when training LLM for trasnlation
+- then the system generalizes - what does it mean when i am asked to translate into a language
+
+page 70:
+- we dont know what training material was for chatgpt
+- for palm it was opensource
+- trained on 750 billion tokens
+- 1.4% bilingual: top 2: french german
+- there is bilinguak and pararell data
+- how systems/LLM are larning bilingual properties
+
+page 71:
+- By adding translated data -> improve translation capabilities
+- into english translation works best
+  - dominant language for training material = english
+  
+page 72:
+advantages of adding LLM on top of MT:
+2) NMT still vrey bound to sentence to sentence translation
+   
+page 74:
+- professional translators do this more - usually 1/2%
+- and here just 0.5%
+- more natural variation coming in
+
+page 76:
+- Gegeargumente:
+  - cultural aspects
+
+- PRoarguemte
+  - huge time and money saver (teachers, etc.)
+  - can strenghten small languages
+
+
+#Intro to Information Retrival
+### Keyword Search
+- page rank algo
+  - to get better search results
+- ^basic idea of google:
+  - if there are a lot of links to one page / visited often => important page
+  
+### Stopwords
+- words that are not relevant for a text
+- => frquent function words
+  - can be removed without losing important information
+- should keep as much as possible tho
+
+### Average Ranked Precision
+- indication for where are the documents with best matches
+- often in exam!!!
+  - should be able to comute this!!
+
+# Benchmarking ChatGPT
+  ### Benchmark Datasets:
+  - worked on evaluating chatgpt
+  - enourmous amount of tests
+  - rouge score = similar to bleu score; wordoverlap; precison and recall based overlapp score
+  - EThics Psychology:
+    - give it the train problem/ ethical dillemas
+  - Open domain Question Answering
+    - like "how high is eiffel tower"
+- Reading Comprehension:
+  - having a text and then asnwering questions to it
+- benchmark test was done, means:
+    - ...
+  - 
+
+### SuperGlue
+- entailment = lexical semantics
+
+
+
+page 14:
+- anything above 20 makes sende
+- eng - kk the worst
+
+### Ethics Benchmark
+- how to measure ethics:
+- chatgpt much better in terms of justice
+- 
---
+++ FS24/PCL1/Course_project/gutenberg_cleanup.py
@@ -0,0 +1,200 @@
+##############################################################################################################
+##############################################################################################################
+##### DO NOT MODIFY THIS CODE #####
+# This code is to be used as is.
+
+import os
+import sys
+import re
+
+# Markers for the start and end of Project Gutenberg headers/footers
+TEXT_START_MARKERS = frozenset((
+    "*END*THE SMALL PRINT",
+    "*** START OF THE PROJECT GUTENBERG",
+    "*** START OF THIS PROJECT GUTENBERG",
+    "This etext was prepared by",
+    "E-text prepared by",
+    "Produced by",
+    "Distributed Proofreading Team",
+    "Proofreading Team at http://www.pgdp.net",
+    "http://gallica.bnf.fr)",
+    "      http://archive.org/details/",
+    "http://www.pgdp.net",
+    "by The Internet Archive)",
+    "by The Internet Archive/Canadian Libraries",
+    "by The Internet Archive/American Libraries",
+    "public domain material from the Internet Archive",
+    "Internet Archive)",
+    "Internet Archive/Canadian Libraries",
+    "Internet Archive/American Libraries",
+    "material from the Google Print project",
+    "*END THE SMALL PRINT",
+    "***START OF THE PROJECT GUTENBERG",
+    "This etext was produced by",
+    "*** START OF THE COPYRIGHTED",
+    "The Project Gutenberg",
+    "http://gutenberg.spiegel.de/ erreichbar.",
+    "Project Runeberg publishes",
+    "Beginning of this Project Gutenberg",
+    "Project Gutenberg Online Distributed",
+    "Gutenberg Online Distributed",
+    "the Project Gutenberg Online Distributed",
+    "Project Gutenberg TEI",
+    "This eBook was prepared by",
+    "http://gutenberg2000.de erreichbar.",
+    "This Etext was prepared by",
+    "This Project Gutenberg Etext was prepared by",
+    "Gutenberg Distributed Proofreaders",
+    "Project Gutenberg Distributed Proofreaders",
+    "the Project Gutenberg Online Distributed Proofreading Team",
+    "**The Project Gutenberg",
+    "*SMALL PRINT!",
+    "More information about this book is at the top of this file.",
+    "tells you about restrictions in how the file may be used.",
+    "l'authorization à les utilizer pour preparer ce texte.",
+    "of the etext through OCR.",
+    "*****These eBooks Were Prepared By Thousands of Volunteers!*****",
+    "We need your donations more than ever!",
+    " *** START OF THIS PROJECT GUTENBERG",
+    "****     SMALL PRINT!",
+    '["Small Print" V.',
+    '      (http://www.ibiblio.org/gutenberg/',
+    'and the Project Gutenberg Online Distributed Proofreading Team',
+    'Mary Meehan, and the Project Gutenberg Online Distributed Proofreading',
+    '                this Project Gutenberg edition.',
+))
+
+
+TEXT_END_MARKERS = frozenset((
+    "*** END OF THE PROJECT GUTENBERG",
+    "*** END OF THIS PROJECT GUTENBERG",
+    "***END OF THE PROJECT GUTENBERG",
+    "End of the Project Gutenberg",
+    "End of The Project Gutenberg",
+    "Ende dieses Project Gutenberg",
+    "by Project Gutenberg",
+    "End of Project Gutenberg",
+    "End of this Project Gutenberg",
+    "Ende dieses Projekt Gutenberg",
+    "        ***END OF THE PROJECT GUTENBERG",
+    "*** END OF THE COPYRIGHTED",
+    "End of this is COPYRIGHTED",
+    "Ende dieses Etextes ",
+    "Ende dieses Project Gutenber",
+    "Ende diese Project Gutenberg",
+    "**This is a COPYRIGHTED Project Gutenberg Etext, Details Above**",
+    "Fin de Project Gutenberg",
+    "The Project Gutenberg Etext of ",
+    "Ce document fut presente en lecture",
+    "Ce document fut présenté en lecture",
+    "More information about this book is at the top of this file.",
+    "We need your donations more than ever!",
+    "END OF PROJECT GUTENBERG",
+    " End of the Project Gutenberg",
+    " *** END OF THIS PROJECT GUTENBERG",
+))
+
+
+LEGALESE_START_MARKERS = frozenset(("<<THIS ELECTRONIC VERSION OF",))
+
+
+LEGALESE_END_MARKERS = frozenset(("SERVICE THAT CHARGES FOR DOWNLOAD",))
+
+def strip_headers(text):
+    """Remove lines that are part of the Project Gutenberg header or footer."""
+    lines = text.splitlines()
+    sep = str(os.linesep)
+
+    out = []
+    i = 0
+    footer_found = False
+    ignore_section = False
+
+    for line in lines:
+        reset = False
+
+        # Header removal
+        if i <= 600 and any(line.startswith(token) for token in TEXT_START_MARKERS):
+            reset = True
+
+        if reset:
+            out = []
+            continue
+
+        # Footer detection
+        if i >= 100 and any(line.startswith(token) for token in TEXT_END_MARKERS):
+            footer_found = True
+
+        if footer_found:
+            break
+
+        # Legalese removal
+        if any(line.startswith(token) for token in LEGALESE_START_MARKERS):
+            ignore_section = True
+            continue
+        elif any(line.startswith(token) for token in LEGALESE_END_MARKERS):
+            ignore_section = False
+            continue
+
+        if not ignore_section:
+            out.append(line.rstrip(sep))
+            i += 1
+
+    return sep.join(out)
+
+##############################################################################################################
+##############################################################################################################
+
+#### MODIFY HERE ####
+
+def split_book_by_chapter(cleaned, book_title):
+    """
+    Implement a function that splits the book into chapters and saves 
+    each chapter in a separate file in a folder named after the book title.
+    """
+    # 1) split book into chapters and save it in a list with regex
+    chapters = re.split(r'\b(?:CHAPTER|Chapter|Letter)\s+(?:\w+\.?\n+)', cleaned)[1:]
+    
+    # 2) splitting the chapters and creating seperate folders for it
+    chapter_num = 0
+    for i in chapters:
+        chapter_name = i.strip() #to filter out the empty spaces - creates list with names and the empty spaces
+        if chapter_name: #if its not just empty
+            chapter_num+= 1
+            file_name = os.path.join(book_title, "Chapters", f"chapter_{chapter_num}.txt")
+            with open(file_name,"w") as f:
+                f.write(i.strip())
+    print("done splitting!")
+
+def main():
+    # 1) check correct input
+    if len(sys.argv) != 2: #2 because argv[0] is the call to programm and argv[1] is the file path
+        print("Incorrect input!")
+        exit(1) #1 to indicate that the program did not execute sucessfully
+        
+
+    # 2) extract information
+    file_path = os.path.abspath(sys.argv[1])
+    book_title = os.path.basename(file_path).replace('.txt', '') 
+    directory = os.path.join(os.path.dirname(file_path), book_title)
+    directory_clean = os.path.join(directory, book_title+"_cleaned.txt") 
+
+    # make folders 
+    os.makedirs(directory)
+    os.makedirs(os.path.join(directory, "Chapters"))
+    
+    # 1. Read the text file
+    with open(file_path, "r", encoding = "utf-8") as f:
+        text = f.read()
+    # 2. Clean the text
+        text_cleaned = strip_headers(text)
+    # 3. Save the cleaned text in the book title folder
+        with open(directory_clean, "w") as dir:
+            dir.write(text_cleaned)
+
+    # 4. Split the text into chapters and save them in the book title folder under a subfolder named 'chapters'
+    split_book_by_chapter(text_cleaned, directory)
+
+if __name__ == '__main__':
+    main()
+    
